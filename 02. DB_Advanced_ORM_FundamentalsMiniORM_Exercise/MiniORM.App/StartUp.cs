1ce547c56cbdb3ca6a4c23ad989cacf39a81e61b
Николай Дойчев <nikolay.doichev@gmail.com>
2019-11-06 22:54:26 +0200

    02. ORM Fundamentals Mini ORM

---
+++ 02. DB_Advanced_ORM_FundamentalsMiniORM_Exercise/MiniORM.App/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MiniORM.App
+{
+    using System.Linq;
+    using Data;
+    using Data.Entities;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;"+
+                "Database=MiniORM;" +
+                "Integrated Security=True;";
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName ="Nikolay",
+                LastName = "Doychev",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChages();
+        }
+    }
+}
---
+++ 02. DB_Advanced_ORM_FundamentalsMiniORM_Exercise/MiniORM/DbSet.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MiniORM
+{
+    public class DbSet<TEntity> : ICollection<TEntity>
+            where TEntity : class, new()
+    {
+        public DbSet(IEnumerable<TEntity> entities)
+        {
+            this.Entities = entities.ToList();
+            this.ChangeTraker = new ChangeTraker<TEntity>(entities);
+        }
+        internal ChangeTraker<TEntity> ChangeTraker { get; set; }
+        internal IList<TEntity> Entities { get; set; }
+
+        public void Add(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item),
+                    "Item cannot be null");
+            }
+
+            this.Entities.Add(item);
+            this.ChangeTraker.Add(item);
+        }
+
+        public void Clear()
+        {
+            while (this.Entities.Any())
+            {
+                TEntity entity = this.Entities.First();
+                this.Remove(entity);
+            }
+        }
+
+        public bool Contains(TEntity item) => this.Entities.Contains(item);
+
+        public void CopyTo(TEntity[] array, int startIndex)
+        {
+            this.Entities.CopyTo(array, startIndex);
+        }
+
+        public int Count => this.Entities.Count;
+
+        public bool IsReadOnly => this.Entities.IsReadOnly;
+
+        public bool Remove(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null");
+            }
+
+            bool removedSuccessfully = this.Entities.Remove(item);
+
+            if (removedSuccessfully)
+            {
+                this.ChangeTraker.Remove(item);
+            }
+
+            return removedSuccessfully;
+        }
+
+        public void RemovedRange(IEnumerable<TEntity> entities)
+        {
+            foreach (TEntity entity in entities.ToArray())
+            {
+                this.Remove(entity);
+            }
+        }
+
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return this.Entities.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
